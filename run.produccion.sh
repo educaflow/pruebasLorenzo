b1c6f3cfa4dba91efa2e0aced3a4601f9a85ed27
logongas <lorenzo.profesor@gmail.com>
2025-07-29 13:00:06 +0200

    Datos de configuración de producción en el repositorio privado "secretaria-virtual-private"

--- run.produccion.sh
+++ run.produccion.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 clear
-export AXELOR_CONFIG="./src/main/resources/axelor-config.production.properties"
+export AXELOR_CONFIG="../secretaria-virtual-private/axelor-config.production.properties"
 ./gradlew clean build
 ./gradlew --no-daemon run --port 8080 --contextPath /
