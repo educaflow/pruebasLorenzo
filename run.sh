dccb865cdc4a9d69aebd64e8663cc29fda591bfe
logongas <lorenzo.profesor@gmail.com>
2025-08-28 14:55:20 +0200

    Script para ejecutar solo los test

--- run.sh
+++ run.sh
@@ -7,6 +7,7 @@ clear
 #docker stop educaflow-db
 #docker run --name educaflow-db --hostname educaflow-db  -e POSTGRES_USER=educaflow -e POSTGRES_PASSWORD=educaflow -e POSTGRES_DB=educaflow -p 5432:5432 -d --rm postgres:12.22
 ./gradlew clean build
+# ./gradlew clean test --info
 #./gradlew --no-daemon run --debug-jvm --port 8080 --contextPath /
 ./gradlew --no-daemon run --port 8080 --contextPath /
 
