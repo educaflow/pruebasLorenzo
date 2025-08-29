03c982bbb10324ae7ea3d049eec9aef734cb46cf
logongas <lorenzo.profesor@gmail.com>
2025-08-30 00:49:47 +0200

    Los datos criptograficos estan en la configuración y se cargan desde el módulo

--- src/main/java/com/educaflow/apps/secretariavirtual/module/SecretariaVirtualModule.java
+++ src/main/java/com/educaflow/apps/secretariavirtual/module/SecretariaVirtualModule.java
@@ -3,13 +3,27 @@ package com.educaflow.apps.secretariavirtual.module;
 import com.axelor.app.AppSettings;
 import com.axelor.app.AvailableAppSettings;
 import com.axelor.app.AxelorModule;
+import com.educaflow.common.criptografia.EntornoCriptografico;
+import com.educaflow.common.criptografia.config.AlmacenCertificadosConfiablesConfig;
+import com.educaflow.common.criptografia.config.ConfiguracionCriptografica;
+import com.educaflow.common.criptografia.config.DispositivoCriptograficoConfig;
 import com.educaflow.common.db.BulkTables;
 
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class SecretariaVirtualModule extends AxelorModule {
 
     protected void configure() {
+
+
+        ConfiguracionCriptografica configuracionCriptografica = getConfiguracionCriptograficaFromAppSettings();
+
+        EntornoCriptografico.configure(configuracionCriptografica);
+
+
         String dataBaseDriver = AppSettings.get().get(AvailableAppSettings.DB_DEFAULT_DRIVER);
         String dataBaseURL = AppSettings.get().get(AvailableAppSettings.DB_DEFAULT_URL);
         String dataBaseUser = AppSettings.get().get(AvailableAppSettings.DB_DEFAULT_USER);
@@ -21,6 +35,26 @@ public class SecretariaVirtualModule extends AxelorModule {
 
         BulkTables bulkTables = new BulkTables();
         bulkTables.truncateTables(dataBaseDriver,dataBaseURL,dataBaseUser,dataBasePassword,schemaName,tablasExcluidas,tablasIncluidas);
+    }
+
+    public ConfiguracionCriptografica getConfiguracionCriptograficaFromAppSettings()  {
+
+        Path pathAlmacen = Path.of(AppSettings.get().get("entornoCriptografico.almacenCertificadosConfiagles.path"));
+        String passwordAlmacen = AppSettings.get().get("entornoCriptografico.almacenCertificadosConfiagles.password");
+        AlmacenCertificadosConfiablesConfig almacenConfig = new AlmacenCertificadosConfiablesConfig(pathAlmacen, passwordAlmacen);
+
+
+        List<DispositivoCriptograficoConfig> dispositivos = new ArrayList<>();
+        int indice=0;
+        while (AppSettings.get().get("entornoCriptografico.dispositivoCriptografico." + indice + ".pkcs11LibraryPath") != null) {
+            Path pkcs11LibraryPath = Path.of(AppSettings.get().get("entornoCriptografico.dispositivoCriptografico." + indice + ".pkcs11LibraryPath"));
+            int slot = Integer.parseInt(AppSettings.get().get("entornoCriptografico.dispositivoCriptografico." + indice + ".slot"));
+            String pin = AppSettings.get().get("entornoCriptografico.dispositivoCriptografico." + indice + ".pin");
+            dispositivos.add(new DispositivoCriptograficoConfig(pkcs11LibraryPath, slot,pin));
+            indice++;
+        }
+
+        return new ConfiguracionCriptografica(almacenConfig, dispositivos);
     }
 
 
