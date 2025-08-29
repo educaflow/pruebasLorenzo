83f26edd490cfba6f7059fed71f086ab961f7c2a
logongas <lorenzo.profesor@gmail.com>
2025-08-29 17:55:27 +0200

    Simplificar el crear un CampoFirma

--- src/main/java/com/educaflow/common/pdf/CampoFirma.java
+++ src/main/java/com/educaflow/common/pdf/CampoFirma.java
@@ -5,17 +5,26 @@ package com.educaflow.common.pdf;
  * @author logongas
  */
 public class CampoFirma {
-    
+
+    public final static int DEFAULT_FONT_SIZE=8;
+
     private final String mensaje;
     private final Rectangulo rectanguloMensaje;
     private final int fontSize;
-    private final int numerpoPagina;
+    private final int numeroPagina;
 
-    public CampoFirma(String mensaje, Rectangulo rectanguloMensaje, int fontSize, int numerpoPagina) {
+    public CampoFirma(String mensaje, Rectangulo rectanguloMensaje, int fontSize, int numeroPagina) {
         this.mensaje = mensaje;
         this.rectanguloMensaje = rectanguloMensaje;
-        this.fontSize = fontSize;
-        this.numerpoPagina = numerpoPagina;
+        this.fontSize = 12;
+        this.numeroPagina = numeroPagina;
+    }
+
+    public CampoFirma(Rectangulo rectanguloMensaje, int numeroPagina) {
+        this.mensaje = null;
+        this.rectanguloMensaje = rectanguloMensaje;
+        this.fontSize = DEFAULT_FONT_SIZE;
+        this.numeroPagina = numeroPagina;
     }
 
     /**
@@ -42,8 +51,8 @@ public class CampoFirma {
     /**
      * @return the numerpoPagina
      */
-    public int getNumerpoPagina() {
-        return numerpoPagina;
+    public int getNumeroPagina() {
+        return numeroPagina;
     }
     
     
